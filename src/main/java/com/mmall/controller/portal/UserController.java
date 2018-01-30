edd260242e43b5f20ccff42a07e8947e384b61b8
Aaron <chen15179095789@163.com>
2018-01-30 22:02:48 +0800

    commit cookie deal

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -32,8 +32,6 @@ public class UserController {
         if(response.isSuccess()) {
             // 登陆成功
             //session.setAttribute(Const.CURRENT_USER , response.getData());
-            CookieUtil.writeLoginToken(httpServletResponse , session.getId());
-            CookieUtil.readLoginToken(httpServletRequest);
             CookieUtil.delLoginToken(httpServletRequest , httpServletResponse);
             RedisPoolUtil.setEx(session.getId() , JsonUtil.obj2String(response.getData()) , Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
         }
